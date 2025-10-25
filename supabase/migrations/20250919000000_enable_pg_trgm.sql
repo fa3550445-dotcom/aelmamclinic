a46e08006d44814e2a4d5a2a3e7bdc62511dcda5
fa3550445-dotcom <fa3550445@gmail.com>
2025-10-26 00:01:16 +0300

    migrations: make pg_trgm creation privilege-safe

--- supabase/migrations/20250919000000_enable_pg_trgm.sql
+++ supabase/migrations/20250919000000_enable_pg_trgm.sql
@@ -1,1 +1,15 @@
-create extension if not exists pg_trgm;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = ''pg_trgm'') THEN
+    RAISE NOTICE ''pg_trgm already installed'';
+  ELSIF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = ''pg_trgm'') THEN
+    BEGIN
+      CREATE EXTENSION IF NOT EXISTS pg_trgm;
+      RAISE NOTICE ''pg_trgm created'';
+    EXCEPTION WHEN insufficient_privilege THEN
+      RAISE NOTICE ''skip pg_trgm creation: insufficient privileges'';
+    END;
+  ELSE
+    RAISE NOTICE ''pg_trgm not available on this server'';
+  END IF;
+END$$;
