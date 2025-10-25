022a6733235b528941328b56b46a2282d64141e8
fa3550445-dotcom <fa3550445@gmail.com>
2025-10-26 00:04:14 +0300

    fix: correct quoting in pg_trgm DO block

--- supabase/migrations/20250919000000_enable_pg_trgm.sql
+++ supabase/migrations/20250919000000_enable_pg_trgm.sql
@@ -1,15 +1,15 @@
 DO $$
 BEGIN
-  IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = ''pg_trgm'') THEN
-    RAISE NOTICE ''pg_trgm already installed'';
-  ELSIF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = ''pg_trgm'') THEN
+  IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pg_trgm') THEN
+    RAISE NOTICE 'pg_trgm already installed';
+  ELSIF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pg_trgm') THEN
     BEGIN
       CREATE EXTENSION IF NOT EXISTS pg_trgm;
-      RAISE NOTICE ''pg_trgm created'';
+      RAISE NOTICE 'pg_trgm created';
     EXCEPTION WHEN insufficient_privilege THEN
-      RAISE NOTICE ''skip pg_trgm creation: insufficient privileges'';
+      RAISE NOTICE 'skip pg_trgm creation: insufficient privileges';
     END;
   ELSE
-    RAISE NOTICE ''pg_trgm not available on this server'';
+    RAISE NOTICE 'pg_trgm not available on this server';
   END IF;
 END$$;
